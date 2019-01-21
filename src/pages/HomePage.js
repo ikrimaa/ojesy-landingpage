af8b082f20559b204dfd6423e9f72c870441e616
ikrimaa _ <ikrimatul@gmail.com>
2019-01-21 07:55:40 +0700

    Escape

---
+++ src/pages/HomePage.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import axios from 'axios';
+import cover from '../components/Cover'
+class HomePage extends Component {
+  state = {
+    menus: [],
+    listPost: []
+  }
+
+  handleLoad = () => {
+    axios.post(`https://private-16c0d2-ikrimaa.apiary-mock.com/v1/landing-page`)
+      .then(res => {
+        let menus = res.data;
+        this.setState({ menus });
+        // console.log(this.state.menus.menu)
+      })
+  }
+
+  handleLoadPost = () => {
+    axios.get('https://private-16c0d2-ikrimaa.apiary-mock.com/v1/landing-page/post')
+      .then(res => {
+        let listPost = res.data;
+        this.setState({
+          listPost
+        });
+        console.log(listPost)
+      })
+  }
+
+  componentDidMount() {
+    this.handleLoad()
+    this.handleLoadPost()
+  }
+
+  render() {
+    const { listPost } = this.state
+    console.log(listPost)  
+    return (
+      <cover />
+    );
+  }
+}
+
+export default HomePage;
--- src/pages/app/AppRouter.js
+++ src/pages/app/AppRouter.js
@@ -1,15 +1,25 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import HomePage from "../base/HomePage";
+import HomePage from "../HomePage"
+import AboutUs from '../AboutUs'
+import NavBar from '../../components/NavBar'
+import NotFound from '../NotFound'
 
 function AppRouter() {
   return (
     <React.StrictMode>
+      <div>
+      <NavBar/>
+      <div>
       <Router>
         <Switch>
-          <Route exact path="/home" component={HomePage} />  
+          <Route exact path="/" component={HomePage} />  
+          <Route  path="/about" component={AboutUs} /> 
+          <Route  component={NotFound}/> 
         </Switch>
       </Router>
+      </div>
+      </div>
     </React.StrictMode>
   );
 }
