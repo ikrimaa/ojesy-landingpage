e1553a405c2ba2f3f489a72362c4d94cd4342ea4
ikrimaa _ <ikrimatul@gmail.com>
2019-01-25 15:27:09 +0700

    add posting material design

--- src/components/PostingCard.js
+++ src/components/PostingCard.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
+
 import './styles.css';
 import Button from './Button';
 import LayoutBody from './LayoutBody';
 import Typography from './Typography';
 import { Card } from '@material-ui/core';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const styles = theme => ({
   root: {
@@ -51,6 +57,12 @@ const styles = theme => ({
       },
     marginTop: theme.spacing.unit * 8,
   },
+  card: {
+    display: 'inline-block',
+    borderRadius: 1,
+    border: 1,
+    boxShadow: 'none',
+  },
 });
 
 function PostingCard(props) {
@@ -68,9 +80,38 @@ function PostingCard(props) {
           BLOG
         </Typography>
         <div>
-         <Card>
-           test
-         </Card>
+        <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              I
+            </Avatar>
+          }
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/paella.jpg"
+          title="Paella dish"
+        />
+        <CardContent>
+          <Typography component="p">
+            This impressive paella is a perfect party dish and a fun meal to cook together with your
+            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.actions} disableActionSpacing>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+        </CardActions>
+      </Card>
         </div>
       </LayoutBody>
     </section>
