612c0a371d3a8fcab2f9b98f0054869c9c39d90c
ikrimaa _ <ikrimatul@gmail.com>
2019-02-11 17:17:54 +0700

    fix home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,7 @@ import SlideCover from '../components/SlideCover'
 import FiturList from '../components/FiturList';
 import Galeri from '../components/Galeri'
 import PostingCard from '../components/PostingCard'
-// import { fadeIn } from 'react-animations';
+import { fadeIn } from 'react-animations';
 import { StyleSheet, css } from 'aphrodite';
  
 
@@ -14,12 +14,12 @@ class HomePage extends React.Component {
  
   
   render() {
-    // const styles = StyleSheet.create({
-    //   fadeIn: {
-    //     animationName: fadeIn,
-    //     animationDuration: '1s'
-    //   }
-    // })
+    const styles = StyleSheet.create({
+      fadeIn: {
+        animationName: fadeIn,
+        animationDuration: '1s'
+      }
+    })
    
     return (
    
