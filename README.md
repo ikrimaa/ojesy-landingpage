98152827f3ffb41e6f1643ad44c188a797b338e5
ikrimaa _ <ikrimatul@gmail.com>
2019-02-13 23:39:42 +0700

    readme membuat project baru

--- README.md
+++ README.md
@@ -1,85 +1,36 @@
-# Landing Page Ojesy 
+# How I built the OJESY landing page with React
 
 ## Overview
+landing page yang dibagun menggunakan react ini memiliki beberapa component, yang akan menampilkan  halaman beranda, tentang kami, blog, dan hubungi kami. Tampilan landing page kali ini menggunakan framework material-ui.com.
 ## Table of Content 
-1. Prerequirement
-2. SPA
-3. React Js
-4. React Router dom v4
-5. Component 
+1. Requirement
+2. Membuat Project Baru
+3. Gitlab Repo
+3. Material Design Dengan Material-ui.com
+4. Membuat Component
+5. Request HTTP
+6. Deploy ke server
+
+## Requirement
+Sebelum Anda membuat landing page dengan react ada beberapa hal yang harus dipersiapkan, seperti :
+
+1. **Text Editor** 
+2. **Node Js** dan **NPM**
+3. **Koneksi Internet**
+
+## Membuat Project Baru 
+untuk membuat project baru dengan react, menggunakan langkah sebagai berikut :
+
+    npx create-react-app ojesy-fe
+    cd ojesy-fe
+    npm start
+
+`create-react-app` tidak menangani logika maupun database , dimana hanya membuat frontend 
+
+berikut tampilan ojesy-fe
+
+## Gitlab Repo
 
 
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
-```json
-{
-    "posting":"test"
-}
-```
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
--- src/pages/app/AppRouter.js
+++ src/pages/app/AppRouter.js
@@ -1,5 +1,51 @@
+// import * as React from "react";
+// import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+// import HomePage from "../HomePage"
+// import AboutUs from '../AboutUs'
+// import NavBar from '../../components/NavBar'
+// import NotFound from '../NotFound'
+// import Footer from "../../components/Footer"
+// import DetailBlog from "../DetailBlog"
+// import BlogList from '../../components/BlogList';
+// import ContactUs from '../ContactUs';
+
+// function AppRouter() {
+//   const publicPath = '/ojesy-landingpage/';
+//   const routeCodes = {
+//     HOME : publicPath,
+//     ABOUT : `${ publicPath }about`,
+//     BLOG : `${ publicPath }blog`,
+//     CONTACT : `${ publicPath }contact`,
+//     DETAIL : `${ publicPath }detail/:detailId`,
+//     NOT : `${ publicPath }NotFound`,
+//   }
+//   return (
+//     <React.StrictMode>
+//       <div>
+//       <NavBar/>
+//       <div>
+//       <Router>
+//         <Switch>
+//           <Route exact path={routeCodes.HOME} component={HomePage} />  
+//           <Route  path={routeCodes.ABOUT} component={AboutUs} /> 
+//           <Route  path={routeCodes.DETAIL} component={DetailBlog} /> 
+//           <Route path={routeCodes.BLOG} component={BlogList}/>
+//           <Route path={routeCodes.NOT} component={NotFound} />
+//           <Route path={routeCodes.CONTACT} component={ContactUs}/>
+//           <Redirect from='*' to={routeCodes.NOT} />
+//         </Switch>
+//       </Router>
+//       </div>
+//       <Footer />
+//       </div>
+//     </React.StrictMode>
+//   );
+// }
+// export default AppRouter;
+
 import * as React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+//import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+ import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import HomePage from "../HomePage"
 import AboutUs from '../AboutUs'
 import NavBar from '../../components/NavBar'
@@ -10,29 +56,29 @@ import BlogList from '../../components/BlogList';
 import ContactUs from '../ContactUs';
 
 function AppRouter() {
-  const publicPath = '/ojesy-landingpage/';
-  const routeCodes = {
-    HOME : publicPath,
-    ABOUT : `${ publicPath }about`,
-    BLOG : `${ publicPath }blog`,
-    CONTACT : `${ publicPath }contact`,
-    DETAIL : `${ publicPath }detail/:detailId`,
-    NOT : `${ publicPath }NotFound`,
-  }
+  // const publicPath = '/ojesy-landingpage/';
+  // const routeCodes = {
+  //   HOME : publicPath,
+  //   ABOUT : `${ publicPath }about`,
+  //   BLOG : `${ publicPath }blog`,
+  //   CONTACT : `${ publicPath }contact`,
+  //   DETAIL : `${ publicPath }detail/:detailId`,
+  //   NOT : `${ publicPath }NotFound`,
+  // }
   return (
     <React.StrictMode>
       <div>
       <NavBar/>
       <div>
-      <Router>
+      <Router basename="/ojesy-landingpage">
         <Switch>
-          <Route exact path={routeCodes.HOME} component={HomePage} />  
-          <Route  path={routeCodes.ABOUT} component={AboutUs} /> 
-          <Route  path={routeCodes.DETAIL} component={DetailBlog} /> 
-          <Route path={routeCodes.BLOG} component={BlogList}/>
-          <Route path={routeCodes.NOT} component={NotFound} />
-          <Route path={routeCodes.CONTACT} component={ContactUs}/>
-          <Redirect from='*' to={routeCodes.NOT} />
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={HomePage} />  
+          <Route  path={`${process.env.PUBLIC_URL}/about`} component={AboutUs} /> 
+          <Route  path={`${process.env.PUBLIC_URL}/detail/:detailId`} component={DetailBlog} /> 
+          <Route path={`${process.env.PUBLIC_URL}/blog`} component={BlogList}/>
+          <Route path={`${process.env.PUBLIC_URL}/NotFound`} component={NotFound} />
+          <Route path={`${process.env.PUBLIC_URL}/contact`} component={ContactUs}/>
+          <Redirect from='*' to={`${process.env.PUBLIC_URL}/NotFound`} />
         </Switch>
       </Router>
       </div>
@@ -42,3 +88,4 @@ function AppRouter() {
   );
 }
 export default AppRouter;
+
