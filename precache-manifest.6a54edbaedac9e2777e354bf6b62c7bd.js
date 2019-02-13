e84eca6843f2e33ab15595ae25164032dc5d3cb4
ikrimaa _ <ikrimatul@gmail.com>
2019-02-13 20:37:32 +0700

    Updates

---
+++ precache-manifest.6a54edbaedac9e2777e354bf6b62c7bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a950dae1f4558d32f130",
+    "url": "/ojesy-landingpage/static/css/main.0f9a5732.chunk.css"
+  },
+  {
+    "revision": "a950dae1f4558d32f130",
+    "url": "/ojesy-landingpage/static/js/main.a950dae1.chunk.js"
+  },
+  {
+    "revision": "9613b330420564d1bd8c",
+    "url": "/ojesy-landingpage/static/css/1.9a25cece.chunk.css"
+  },
+  {
+    "revision": "9613b330420564d1bd8c",
+    "url": "/ojesy-landingpage/static/js/1.9613b330.chunk.js"
+  },
+  {
+    "revision": "2ab4356c1984708263b3",
+    "url": "/ojesy-landingpage/static/js/runtime~main.2ab4356c.js"
+  },
+  {
+    "revision": "3f5f5315116244e02ec74db07ee37a8b",
+    "url": "/ojesy-landingpage/static/media/logo.3f5f5315.png"
+  },
+  {
+    "revision": "c18cf0729f7eaa799d4e6e9068bd8d3a",
+    "url": "/ojesy-landingpage/static/media/instagram-logo.c18cf072.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ojesy-landingpage/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "3e7c50b8c15194ce4eac3eeb2b0a0662",
+    "url": "/ojesy-landingpage/index.html"
+  }
+];
